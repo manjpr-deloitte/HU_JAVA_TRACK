c7abc100af9dca0f5abf39588ccddee457f25595
manjpr-deloitte <101085428+manjpr-deloitte@users.noreply.github.com>
2022-03-21 15:50:06 +0530

    Add files via upload

---
+++ 1.OOPS_ASSIGNMENT/src/overload.java
@@ -0,0 +1,11 @@
+public class overload {
+    public static void main(String args[])
+    {
+        overloaddemo ob = new overloaddemo();
+        overload ob1= new overload();
+        ob.area(2.5);
+        ob.area(5);
+        ob.area(11,12);
+        ob.areatriangle(10,5);
+    }
+}
